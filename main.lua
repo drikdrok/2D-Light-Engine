60e0d4b9890abdd8409182926039d649ffda84c9
Christian Schwenger <drikdrok02@gmail.com>
2021-04-27 23:22:44 +0200

    cleanup

--- main.lua
+++ main.lua
@@ -71,13 +71,7 @@ function love.load()
 	visibillityPolygon = {}
 
 
-
---	test = {{3, 5, 4}, {1, 3, 10}, {4, 1, 0}, {3, 1, 100}}
-	--table.sort(test, function(a, b) return a[3] < b[3] end )
-	--error(serialize_list(test), 4)
-
 	lightMap = love.graphics.newCanvas(800, 600)
-
 end
 
 function love.update(dt)
@@ -85,10 +79,6 @@ function love.update(dt)
 
 	if love.mouse.isDown(1) then 
 		if mouseDown then 
-			rectangles[#rectangles].width = rectangles[#rectangles].width + 1
-			rectangles[#rectangles].height = rectangles[#rectangles].height + 1
-
-
 			--Expand edges of currently expanding square
 			edges[#edges-3].endX = edges[#edges-3].endX + 1
 		
@@ -103,13 +93,10 @@ function love.update(dt)
 			edges[#edges].endY = edges[#edges].endY + 1
 		else
 			mouseDown = true
-			table.insert(rectangles, {x = love.mouse.getX(), y = love.mouse.getY(), width = 16, height = 16})
-			
-
-			table.insert(edges, {x = mouseX, y = mouseY, endX = mouseX + 16, endY = mouseY}) 
-			table.insert(edges, {x = mouseX, y = mouseY, endX = mouseX, endY = mouseY + 16}) 
+			table.insert(edges, {x = mouseX + 16, y = mouseY, endX = mouseX + 32, endY = mouseY}) 
 			table.insert(edges, {x = mouseX + 16, y = mouseY, endX = mouseX + 16, endY = mouseY + 16}) 
-			table.insert(edges, {x = mouseX, y = mouseY + 16, endX = mouseX + 16, endY = mouseY + 16}) 
+			table.insert(edges, {x = mouseX + 32, y = mouseY, endX = mouseX + 32, endY = mouseY + 16}) 
+			table.insert(edges, {x = mouseX + 16, y = mouseY + 16, endX = mouseX + 32, endY = mouseY + 16}) 
 		end
 	else
 		mouseDown = false
@@ -201,11 +188,9 @@ end
 function calculateVisibillityPolygon(sourceX, sourceY, radius)
 	visibillityPolygon = {}
 
-	for i, edge1 in pairs(edges) do 
-			-- Take the start point, then the end point (we could use a pool of
-			-- non-duplicated points here, it would be more optimal)
+	for i, edge1 in pairs(edges) do -- Loop through every edge
 			for j = 0, 2 do 
-				local rayVectorX = edge1.x - sourceX
+				local rayVectorX = edge1.x - sourceX -- Target either start or end point of edge
 				local rayVectorY = edge1.y - sourceY
 				if j == 1 then 
 					 rayVectorX = edge1.endX - sourceX
@@ -214,85 +199,46 @@ function calculateVisibillityPolygon(sourceX, sourceY, radius)
 
 				local baseAngle = math.atan2(rayVectorY, rayVectorX);
 
-				local angle = 0;
-				-- For each point, cast 3 rays, 1 directly at point
-				-- and 1 a little bit either side
+				local angle = 0
 				for k = 0, 3 do 
 					if k == 0 then angle = baseAngle - 0.0001 end
 					if k == 1 then angle = baseAngle 		   end
 					if k == 2 then angle = baseAngle + 0.0001 end
 
-					-- Create ray along angle for required distance
-					rayVectorX = radius * math.cos(angle);
-					rayVectorY = radius * math.sin(angle);
+					rayVectorX = radius * math.cos(angle)
+					rayVectorY = radius * math.sin(angle)
 
-					local closest_t1 = 99999999;
+					local closest_t1 = 99999999
 					local closestPointX = 0
 					local closestPointY = 0
 					local closestAngle = 0
-					local isValid = false;
+					local isValid = false
 
-					-- Check for ray intersection with all edges
-					for l, e2 in pairs(edges) do
-						-- Create line segment vector
-						local sdx = e2.endX - e2.x;
-						local sdy = e2.endY - e2.y;
+					for l, edge2 in pairs(edges) do
+						local sdx = edge2.endX - edge2.x
+						local sdy = edge2.endY - edge2.y
 
-						if (math.abs(sdx - rayVectorX) > 0.0 and math.abs(sdy - rayVectorY) > 0.0) then 
-							-- t2 is normalised distance from line segment start to line segment end of intersect point
-							local t2 = (rayVectorX * (e2.y - sourceY) + (rayVectorY * (sourceX - e2.x))) / (sdx * rayVectorY - sdy * rayVectorX);
-							-- t1 is normalised distance from source along ray to ray length of intersect point
-							local t1 = (e2.x + sdx * t2 - sourceX) / rayVectorX;
+						if math.abs(sdx - rayVectorX) > 0.0 and math.abs(sdy - rayVectorY) > 0.0 then -- Make sure vectors are not parralel
+							local t2 = (rayVectorX * (edge2.y - sourceY) + (rayVectorY * (sourceX - edge2.x))) / (sdx * rayVectorY - sdy * rayVectorX)
+							local t1 = (edge2.x + sdx * t2 - sourceX) / rayVectorX
 
-							-- If intersect point exists along ray, and along line 
-							-- segment then intersect point is valid
-							if (t1 > 0 and t2 >= 0 and t2 <= 1.0) then 
-								-- Check if this intersect point is closest to source. If
-								-- it is, then store this point and reject others
+							if (t1 > 0 and t2 >= 0 and t2 <= 1.0) then -- Intersect point is valid
 								if (t1 < closest_t1) then 
-									closest_t1 = t1;
-									closestPointX = sourceX + rayVectorX * t1;
-									closestPointY = sourceY + rayVectorY * t1;
-									closestAngle = math.atan2(closestPointY - sourceY, closestPointX - sourceX);
-									isValid = true;
+									closest_t1 = t1
+									closestPointX = sourceX + rayVectorX * t1
+									closestPointY = sourceY + rayVectorY * t1
+									closestAngle = math.atan2(closestPointY - sourceY, closestPointX - sourceX)
+									isValid = true
 								end
 							end
 						end
 					end
 
-					if(isValid) then -- Add intersection point to visibility polygon perimeter
-						--vecVisibilityPolygonPoints.push_back({ closestAngle, closestPointX, closestPointY });
+					if isValid then 
 						table.insert(visibillityPolygon, {closestPointX, closestPointY, closestAngle})
 					end
 				end
 			end
 		end
-
-
-	table.sort(visibillityPolygon, function(a, b) return a[3] < b[3] end )
-	--error(serialize_list(visibillityPolygon), 3)
-	local test = {}
-	for i,v in pairs(visibillityPolygon) do
-		table.insert(test, v[3])
-	end	
-end
-
-
-local nl = string.char(10) -- newline
-function serialize_list (tabl, indent)
-    indent = indent and (indent.."  ") or ""
-    local str = ''
-    str = str .. indent.."{"..nl
-    for key, value in pairs (tabl) do
-        local pr = (type(key)=="string") and ('["'..key..'"]=') or ""
-        if type (value) == "table" then
-            str = str..pr..serialize_list (value, indent)
-        elseif type (value) == "string" then
-            str = str..indent..pr..'"'..tostring(value)..'",'
-        else
-            str = str..indent..pr..tostring(value)..','
-        end
-    end
-    str = str .. indent.."},".. nl
-    return str
+	table.sort(visibillityPolygon, function(a, b) return a[3] < b[3] end ) -- Sort points by angle
 end
